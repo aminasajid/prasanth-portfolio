427afbd8f50a6c124b308792c228b973e6cdada5
aminasajid <aminasajid05@gmail.com>
2025-09-16 11:28:56 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className="px-4 md:px-[100px] pt-[40px] md:pt-[57px] pb-[20px] md:pb-[30px]">
+    <header className="hidden lg:block px-4 md:px-[100px] pt-[40px] md:pt-[57px] pb-[20px] md:pb-[30px]">
       <div className="flex flex-col md:flex-row items-center justify-between gap-6 md:gap-0">
         {/* Left Menu */}
         <div className="flex gap-[30px] md:gap-[53px] font-syne text-[#555] text-base md:text-xl font-bold tracking-[-0.4px] md:tracking-[-0.64px]">
